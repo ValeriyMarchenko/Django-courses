257ba8037e58e593edd43a9be59d392899ea74ae
Valeriy <valeriy.marchenko02@gmail.com>
2022-03-16 13:23:32 +0300

    Table-view of news added

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,10 +1,17 @@
 from django.views.generic import ListView, DetailView
 from .models import Category, Post
+from datetime import datetime
 
 class PostList(ListView):
     model = Post
     template_name = 'posts.html'
     context_object_name = 'posts'
+
+    def get_contex_data(self, **kwargs):
+        contex = super().get_contex_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['value1'] = None
+        return contex
 
 class PostDetail(DetailView):
     model = Post
