99722e98196de0664590579add1adfcfe56a361d
Valeriy <valeriy.marchenko02@gmail.com>
2022-03-17 01:15:48 +0300

    Censor func rework

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -5,7 +5,11 @@ register = template.Library()
 ban_words = ['nigga', 'amogus', 'fuck']
 
 @register.filter(name='censor')  
-def censor(value):  
+def censor(value): 
+    if not isinstance(value, str):
+        raise ValueError('Cant censor not str object') 
     for i in ban_words:
-        value = value.replace(i, '*' * len(i) )
+        value = value.replace(i[0:], '*' * len(i) )
     return value 
+
+# alternative var. :  value = value.replace(i[1:], '*' * (len(i) - 1) )
