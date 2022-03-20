2612cf01fa94e3a50a3b76969defbe1ef2640ab6
Valeriy <valeriy.marchenko02@gmail.com>
2022-03-20 15:17:29 +0300

    Searching func update

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,24 +1,23 @@
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 from datetime import datetime
+from .filters import PostFilter
 
 class PostList(ListView):
     model = Post
     template_name = 'posts.html'
     context_object_name = 'posts'
-    queryset = Post.objects.order_by('-dateCreation')
+    ordering = '-dateCreation'
+    paginate_by = 1
 
-    def get_context_data(self, **kwargs):
+    def get_time(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
         return context
 
-class PostDetail(DetailView):
-    model = Post
-    template_name = 'post.html'
-    context_object_name = 'post'
-
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):  # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса (привет, полиморфизм, мы скучали!!!)
         context = super().get_context_data(**kwargs)
-        context['time_now'] = datetime.utcnow()
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
         return context
+
