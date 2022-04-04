d21a2b158ede56ce4d86161e80d017e56737cc0f
Valeriy <valeriy.marchenko02@gmail.com>
2022-04-04 14:51:37 +0300

    Google auth improve

--- NewsPaper/sign/models.py
+++ NewsPaper/sign/models.py
@@ -4,6 +4,9 @@ from django import forms
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import Group
 
+from allauth.account.signals import user_signed_up
+from django.dispatch import receiver
+
 
 class BasicSignupForm(SignupForm):
     
@@ -12,3 +15,8 @@ class BasicSignupForm(SignupForm):
         basic_group = Group.objects.get(name='common')
         basic_group.user_set.add(user)
         return user
+
+@receiver(user_signed_up)
+def user_signed_up_(request, user, **kwargs):
+    Group.objects.get(name='common').user_set.add(user)
+    user.save()
